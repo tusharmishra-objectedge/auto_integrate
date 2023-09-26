fe77143413ef3d6473e2b62b66039ef8383bd6d5
aditya17varma <69626061+aditya17varma@users.noreply.github.com>
2023-09-26 11:26:39 -0700

    python script to test get request for mockAPI

---
+++ mockAPI_test.py
@@ -0,0 +1,26 @@
+import requests
+
+url = "https://651313e18e505cebc2e98c43.mockapi.io"
+
+try:
+    getStudents = url + "/students"
+    getPupils = url + "/pupils"
+
+    print(f'getStudents: {getStudents}')
+
+    response = requests.get(getStudents)
+
+    if response.status_code == 200:
+        print("GET students: OK")
+        data = response.json()
+        # print(data)
+
+        if len(data) > 0:
+            for key, value in data[0].items():
+                data_type = type(value).__name__
+                print(f'{key}: ({data_type})')
+
+    else:
+        print("MockAPI GET: ERROR ", response.status_code)
+except Exception as e:
+    print(f'Error: {e}')
