6a321118f96b78f2ce45b8c2f36bceb55d2fe1c3
aditya17varma <69626061+aditya17varma@users.noreply.github.com>
2023-09-29 12:10:22 -0700

    testing OpenAI api call with mock apis

--- app/openAPI/openAPI.py
+++ app/openAPI/openAPI.py
@@ -24,7 +24,8 @@ def match_api_fields(api1, api2):
     response = openai.Completion.create(
         engine="text-davinci-003",
         prompt=prompt,
-        max_tokens=150
+        max_tokens=150,
+        temperature=0.5,
     )
 
     generated_mapping = response.choices[0].text.strip()
@@ -38,24 +39,63 @@ def match_api_fields(api1, api2):
 
     return field_mapping
 
+def calculate_metrics(api1, api2, resultFile):
+    with open(resultFile) as f:
+        data = json.load(f)
+
+
+    num_fields1 = len(api1)
+    num_fields2 = len(api2)
+    print(f'api1 has {num_fields1} fields')
+    print(f'api2 has {num_fields2} fields')
+
+    api1_fields = {k.replace(" ","").lower() for k in api1.keys()}
+    api2_fields = {k.replace(" ","").lower() for k in api2.keys()}
+
+    print(f'api1_fields: {api1_fields}')
+    print(f'api2_fields: {api2_fields}')
+    print()
+
+    matches = 0
+
+    for api1res, api2res in data.items():
+        api1resClean = api1res.replace(" ","").lower()
+        api2resClean = api2res.replace(" ", "").lower()
+        print(f'api1resClean: {api1resClean} api2resClean: {api2resClean}')
+        if api1resClean in api1_fields and api2resClean in api2_fields:
+            api1_fields.remove(api1resClean)
+            api2_fields.remove(api2resClean)
+            matches += 1
+
+    print()
+    print(f'For {num_fields1} in api1, and {num_fields2} in api2, we got {matches} matches')
 
 
 if __name__ == "__main__":
 
     input_file = os.path.join("../../demo", "inputs/mockAPI1.json")
-    # file1 = JSONHandler("../demo/inputs/mockAPI1.json")
     file1 = JSONHandler(input_file)
     inputs = file1.read()
-    # print(inputs)
 
     api_formatter = APIFormatter(inputs)
     api1, api2 = api_formatter.format()
 
-    # print(f"\nAPI 1: {api1}\n")
-    # print(f"API 2: {api2}\n")
+    print('Mapping api1 to api2')
 
     result = match_api_fields(api1, api2)
 
 
     print('OpenAI returned: ')
     print(result)
+
+    if len(result) > 0:
+        # save to output file
+        with open('openAI_output.txt', 'w+') as f:
+            json.dump(result, f, indent=2)
+
+        print('Wrote output to file')
+
+        calculate_metrics(api1, api2, 'openAI_output.txt')
+    else:
+        print('OpenAI returned no results, run again')
+
