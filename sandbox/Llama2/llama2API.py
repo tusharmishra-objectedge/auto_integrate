c7710e843f8acc374a9e677a2da648519451fa0e
aditya17varma <69626061+aditya17varma@users.noreply.github.com>
2023-10-11 11:28:06 -0700

    llama2 api

--- sandbox/Llama2/llama2API.py
+++ sandbox/Llama2/llama2API.py
@@ -1,17 +1,26 @@
 import replicate
 import os
 
+if __name__ == "__main__":
+    os.environ["REPLICATE_API_TOKEN"] = "r8_QYWVuQFgraS9EELRwOy60Fe3aNkoEvp2oanCC"
 
-os.environ["REPLICATE_API_TOKEN"] = "r8_QYWVuQFgraS9EELRwOy60Fe3aNkoEvp2oanCC"
+
+    input_file = os.path.join("../../demo", "inputs/mockAPI1.json")
+    file1 = JSONHandler(input_file)
+    inputs = file1.read()
+
+    api_formatter = APIFormatter(inputs)
+    api1, api2 = api_formatter.format()
 
 
-output = replicate.run(
-    "meta/llama-2-70b-chat:2c1608e18606fad2812020dc541930f2d0495ce32eee50074220b87300bc16e1",
-    input={"prompt": "Write a poem about open source machine learning in the style of Mary Oliver."}
-)
-# The meta/llama-2-70b-chat model can stream output as it's running.
-# The predict method returns an iterator, and you can iterate over that output.
-for item in output:
-    print(item)
+
+    output = replicate.run(
+        "meta/llama-2-70b-chat:2c1608e18606fad2812020dc541930f2d0495ce32eee50074220b87300bc16e1",
+        input={"prompt": f'Match the fields from api1 to those in api2. The data is given in a JSON format, with the keys being the field and the values the type of the data. api1: {api1} and api2: {api2}.'}
+    )
+    # The meta/llama-2-70b-chat model can stream output as it's running.
+    # The predict method returns an iterator, and you can iterate over that output.
+    for item in output:
+        print(item)
 
 
